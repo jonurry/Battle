d18304bd24d6701b4bbc41860c78782b30bc6915
Jon Urry <jon@urry.me>
2018-09-21 16:48:18 +0100

    Killing globals

--- app.rb
+++ app.rb
@@ -12,23 +12,25 @@ class Battle < Sinatra::Base
   post '/names' do
     player_1 = Player.new(params[:player_1])
     player_2 = Player.new(params[:player_2])
-    $game = Game.new(player_1, player_2)
+    Game.create(player_1, player_2)
     redirect '/play'
   end
 
   get '/play' do
-    @player_1 = $game.player_1.name
-    @player_2 = $game.player_2.name
+    nice_game = Game.instance
+    @player_1 = nice_game.player_1.name
+    @player_2 = nice_game.player_2.name
     erb(:play)
     #binding.pry
   end
 
   get '/attack' do
-    @player_1 = $game.player_1.name
-    @player_2 = $game.player_2.name
-    $game.attack($game.player_2)
-    @health_1 = $game.player_1.health
-    @health_2 = $game.player_2.health
+    nice_game = Game.instance
+    @player_1 = nice_game.player_1.name
+    @player_2 = nice_game.player_2.name
+    nice_game.attack(nice_game.player_2)
+    @health_1 = nice_game.player_1.health
+    @health_2 = nice_game.player_2.health
     erb :attack
   end
 
